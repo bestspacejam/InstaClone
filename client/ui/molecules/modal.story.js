2a3135356f4de53a48fe2e4177959cdc93d8ceed
Andrey Savelev <sandrig.dev@gmail.com>
2018-03-19 22:08:18 +0300

    refactor(client/ui): add state for modal component in storybook

--- client/ui/molecules/modal.story.js
+++ client/ui/molecules/modal.story.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { text } from '@storybook/addon-knobs'
+import { text, boolean } from '@storybook/addon-knobs'
 
 import { Modal } from './modal'
 
 
+class WithState extends Component {
+  state = { modalOpened: false }
+
+  onClose = () => {
+    this.setState(() => ({ modalOpened: true }))
+  }
+
+  render() {
+    return this.props.children({
+      onClose: this.onClose,
+      opened: this.state.modalOpened,
+    })
+  }
+}
+
 storiesOf('ui/molecules', module)
   .addWithJSX('Modal', () => (
-    <Modal onClose={action('Close')}>
-      <div style={{ fontSize: '1.4rem', color: '#fff' }}>{text('Children', 'Example content')}</div>
-    </Modal>
-  ))
+    <WithState default="Example">
+      {(props) => (
+        <Modal opened={boolean('Opened', true)} onClose={action('Close')} {...props}>
+          <div style={{ fontSize: '1.4rem', color: '#fff' }}>{text('Children', 'Example content')}</div>
+        </Modal>
+      )}
+    </WithState>
+  ), { skip: 1 })
